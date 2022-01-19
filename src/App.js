e7328ce457d4b7d60afa354185a9bdbf932dbabe
Jorge Gurgel <jlgurgelfernandes@gmail.com>
2022-01-19 19:41:00 -0300

    correction section5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/pages/header/Header";
 import Section1 from "./components/pages/section1/Section1";
 import Section2 from "./components/pages/section2/Section2";
 import Section3 from "./components/pages/section3/Section3";
 import Section4 from "./components/pages/section4/Section4";
-import Section5 from "./components/pages/section5/Section5";
 
-function App() {
-  const [cond, setCond] = useState(false);
-  setCond(false);
+export default function App() {
   return (
     <>
       <Header />
@@ -16,10 +13,6 @@ function App() {
       <Section2 />
       <Section3 />
       <Section4 />
-
-      {cond && <Section5 />}
     </>
   );
 }
-
-export default App;
